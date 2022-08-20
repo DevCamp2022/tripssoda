eb23b168274e274ae7dfa04f72f142d27a75703f
Renee <gracekang61@naver.com>
2022-08-20 21:36:22 +0900

    [Feature] 게시판 이력관련추가, 테스트코드

--- src/test/java/com/devcamp/tripssoda/mapper/AdminBoardMapperTest.java
+++ src/test/java/com/devcamp/tripssoda/mapper/AdminBoardMapperTest.java
@@ -8,28 +8,25 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import static org.junit.Assert.*;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+@RunWith(SpringJUnit4ClassRunner.class) //ac를자동으로 만들어주는애.
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"}) //xml설정파일 위치를 지정해주는것
 @WebAppConfiguration
 public class AdminBoardMapperTest {
     @Autowired
-    private AdminBoardMapper adminBoardMapper;
+    private CombinedBoardMapper combinedBoardMapper;
 
-//    public AdminBoardMapperTest(){}
-//    public AdminBoardMapperTest(AdminBoardMapper adminBoardMapper){
-//        this.adminBoardMapper = adminBoardMapper;
-//    }
+    public AdminBoardMapperTest(){}
 
     @Test
     public void insertTestData() throws Exception{
 //        adminBoardMapper.deleteAll();
-        for(int i=1; i<=100; i++){
-            CombinedBoardDto combinedBoardDto = new CombinedBoardDto("M00"+(i%3+1),41, "제목제목", "내용내용",41);
-            adminBoardMapper.insert(combinedBoardDto);
+        for(int i=1; i<=10; i++){
+            CombinedBoardDto combinedBoardDto = new CombinedBoardDto("M00"+(i%3+1),41, "제목제목", "내용내용",41,41);
+            combinedBoardMapper.insert(combinedBoardDto);
         }
     }
+
+
 
 
 }

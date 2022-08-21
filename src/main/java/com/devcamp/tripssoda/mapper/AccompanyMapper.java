9de08e5b3b080d0e1753ada9f6586c566f81213b
Darren Gwon <darren.gwon@gmail.com>
2022-08-21 16:35:30 +0900

    [Conflict] AccompanyMapper.java

--- src/main/java/com/devcamp/tripssoda/mapper/AccompanyMapper.java
+++ src/main/java/com/devcamp/tripssoda/mapper/AccompanyMapper.java
@@ -8,12 +8,8 @@ import java.util.Map;
 
 @Mapper
 public interface AccompanyMapper {
-<<<<<<< HEAD
     public int selectUserAccompanyCnt(Integer userId);
-
     public List<AccompanyDto> selectAllUserAccompany(Map map);
-
-=======
     public int count() throws Exception;
     public int insert(AccompanyDto dto) throws Exception;
     public AccompanyDto select(Integer id) throws Exception;
@@ -23,5 +19,4 @@ public interface AccompanyMapper {
     public int deleteAll() throws Exception;
     public List<AccompanyDto> selectPage(Map map);
     public List<AccompanyDto> selectAll();
->>>>>>> 52156582d8c8ea7cfd06c7ae5802c09bf693e45e
 }

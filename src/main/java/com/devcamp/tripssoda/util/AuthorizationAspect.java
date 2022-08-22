b19e64c08f20a2475fbe56e2b3ea102bfbd3ffef
Renee <gracekang61@naver.com>
2022-08-22 09:13:17 +0900

    [Fix] 페이지에대한 권한체크 사용자아이디 -> 그룹코드로 변경, aop 포인트컷을 유저를 일반,파트너 로 분리

--- src/main/java/com/devcamp/tripssoda/util/AuthorizationAspect.java
+++ src/main/java/com/devcamp/tripssoda/util/AuthorizationAspect.java
@@ -1,6 +1,7 @@
 package com.devcamp.tripssoda.util;
 
 import com.devcamp.tripssoda.controller.AdminController;
+import com.devcamp.tripssoda.mapper.UserMapper;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -19,28 +20,46 @@ public class AuthorizationAspect {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthorizationAspect.class);
 
-    @Around("execution(* com.devcamp.tripssoda.controller.AdminController.*(..)) ||" +
-            "execution(* com.devcamp.tripssoda.controller.PartnerController.*(..)) " +
-            "&& !@annotation(com.devcamp.tripssoda.util.SkipChecking)" )
-
-    public Object idCheck(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("execution(* com.devcamp.tripssoda.controller.AdminController.*(..))" +
+            "&& !@annotation(com.devcamp.tripssoda.util.annotations.SkipChecking)"
+            + "|| @annotation(com.devcamp.tripssoda.util.annotations.AuthChecking)")
+    public Object AdminCheck(ProceedingJoinPoint joinPoint) throws Throwable {
         logger.info("### {}: before", joinPoint.getSignature().getName());
-        System.out.println("\"AOP - Member Login Check Started\" = " + "AOP - Member Login Check Started");
-
+        System.out.println("\"AOP - Admin Login Check Started\" = " + "AOP - Admin Login Check Started");
 
         HttpSession session = ((ServletRequestAttributes)(RequestContextHolder.currentRequestAttributes())).getRequest().getSession();
-        Integer userId = SessionUtil.getLoginMemberId(session);
+        String userCode = SessionUtil.getLoginUserCode(session);
 
-        if(userId==null){
-            logger.info("sessionId is not present. Go to login page");
-            System.out.println("sessionId is not present. Go to login page");
-//            AdminController.toLoginPage();
+        if(userCode==null || !userCode.equals("U003")){
+            logger.info("AdminCode is not present. Go to login page");
+            System.out.println("AdminCode Code is not present. Go to login page");
             return "redirect:/";
         }
-
         logger.info("### {}: after\n", joinPoint.getSignature().getName());
         Object result = joinPoint.proceed();
         return result;
     }
+
+    @Around("execution(* com.devcamp.tripssoda.controller.PartnerController.*(..)) " +
+            "&& !@annotation(com.devcamp.tripssoda.util.annotations.SkipChecking)"
+            + "|| @annotation(com.devcamp.tripssoda.util.annotations.AuthChecking)")
+    public Object partnerCheck(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("### {}: before", joinPoint.getSignature().getName());
+        System.out.println("\"AOP - Partner Login Check Started\" = " + "AOP - Partner Login Check Started");
+
+        HttpSession session = ((ServletRequestAttributes)(RequestContextHolder.currentRequestAttributes())).getRequest().getSession();
+        String userCode = SessionUtil.getLoginUserId(session);
+
+        if(userCode==null || !userCode.equals("U002")){
+            logger.info("partnerCode is not present. Go to login page");
+            System.out.println("partnerCode Code is not present. Go to login page");
+            return "redirect:/";
+        }
+        logger.info("### {}: after\n", joinPoint.getSignature().getName());
+        Object result = joinPoint.proceed();
+        return result;
+    }
+
+
 
 }

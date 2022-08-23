3c5f9b586a2ea85512c1d64cd4c55a71381d3cb7
Darren Gwon <darren.gwon@gmail.com>
2022-08-23 16:16:05 +0900

    [Feature] 결제 성공 후 받는 message 로직 수정 밑 결제완료페이지로 이동하도록 반영

--- src/main/java/com/devcamp/tripssoda/controller/PaymentController.java
+++ src/main/java/com/devcamp/tripssoda/controller/PaymentController.java
@@ -2,14 +2,11 @@ package com.devcamp.tripssoda.controller;
 
 import com.devcamp.tripssoda.dto.PaymentDetailDto;
 import com.devcamp.tripssoda.dto.PaymentInitialInfoDto;
+import com.devcamp.tripssoda.dto.PaymentSuccessDto;
 import com.devcamp.tripssoda.service.PaymentService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.siot.IamportRestClient.IamportClient;
-import com.siot.IamportRestClient.exception.IamportResponseException;
-import com.siot.IamportRestClient.response.IamportResponse;
-import com.siot.IamportRestClient.response.Payment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,8 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/payment")
@@ -48,11 +45,12 @@ public class PaymentController {
 
     @PostMapping("/complete")
     @ResponseBody
-    public ResponseEntity<String> complete(@RequestBody String paymentDetail, HttpSession session) {
+    public ResponseEntity<Map<String, Object>> complete(@RequestBody String paymentDetail, HttpSession session) {
 
         PaymentDetailDto paymentDetailDto = null;
         ObjectMapper objectMapper = new ObjectMapper()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        Map<String, Object> result = new HashMap<>();
 
         try {
             paymentDetailDto = objectMapper.readValue(paymentDetail, PaymentDetailDto.class);
@@ -64,33 +62,33 @@ public class PaymentController {
             paymentService.insertPayment(paymentDetailDto);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
-            return new ResponseEntity<String>("결제 정보가 누락되었습니다.", HttpStatus.BAD_REQUEST);
+            result.put("message","결제 정보가 누락되었습니다.");
+            return new ResponseEntity<Map<String, Object>>(result, HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             e.printStackTrace();
 
             switch (e.getMessage()) {
-                case "code: P100":
-                    return new ResponseEntity<String>("결제 정보가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
-                case "code: P200":
-                    return new ResponseEntity<String>("보유하신 포인트가 부족합니다.", HttpStatus.BAD_REQUEST);
-                case "code: P300":
-                    return new ResponseEntity<String>("결제 처리도중 에러가 발생하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
-                default:
-                    return new ResponseEntity<String>("결제에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+                case "code: P100": {
+                    result.put("message", "결제 정보가 일치하지 않습니다.");
+                    return new ResponseEntity<Map< String,Object>>(result, HttpStatus.BAD_REQUEST);
+                }
+                case "code: P200":{
+                    result.put("message", "보유하신 포인트가 부족합니다.");
+                    return new ResponseEntity<Map< String,Object>>(result, HttpStatus.BAD_REQUEST);
+                }
+                case "code: P300": {
+                    result.put("message", "결제 처리도중 에러가 발생하였습니다.");
+                    return new ResponseEntity<Map<String, Object>>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+                default: {
+                    result.put("message", "결제에 실패했습니다.");
+                    return new ResponseEntity<Map<String, Object>>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+                }
             }
         }
-        return new ResponseEntity<String>("payment success", HttpStatus.OK);
+        result.put("message","payment success");
+        result.put("paymentId", paymentDetailDto.getId());
+        return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
     }
-
-    @GetMapping("/success")
-    public String success(HttpSession session, String reservationId){
-        Integer userId = (Integer) session.getAttribute("id");
-        return "payment/success.mainTiles";
-    }
-
-//    @PostMapping
-//    public IamportResponse<Payment> paymentByImpUid(Integer imp_uid) throws IamportResponseException, IOException {
-//        return importClient.paymentByImpUid(imp_uid);
-//    }
 
 }
--- src/main/webapp/resources/script/payment/importApi.js
+++ src/main/webapp/resources/script/payment/importApi.js
@@ -26,16 +26,16 @@ $("#payBtn").click(function () {
             paymentDetailObj.receiptUrl = rsp.receipt_url;
             if (rsp.success) { // 결제 성공 시: 결제 승인 또는 가상계좌 발급에 성공한 경우
                 // jQuery로 HTTP 요청
-
                 jQuery.ajax({
                     url: "/payment/complete", // 예: https://www.myservice.com/payments/complete
                     method: "POST",
                     headers: {"Content-Type": "application/json; charset=UTF-8"},
                     data: JSON.stringify(paymentDetailObj),
                 }).done(function (data) {
-                    if(data!=="payment success"){
+                    if(data.message!=="payment success"){
                         alert("결제에 실패했습니다. 다시 시도해주시기 바랍니다.");
                     }
+                    location.href="/payment/success?paymentId="+data.paymentId;
                 })
             } else {
                 alert("결제에 실패하였습니다. 에러 내용: " + rsp.error_msg);

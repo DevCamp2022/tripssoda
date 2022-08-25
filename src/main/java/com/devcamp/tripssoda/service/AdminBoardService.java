0984b89b3fac71e4c43a047bd65432ac2938167b
Renee <gracekang61@naver.com>
2022-08-25 10:39:33 +0900

    [Feature] 1:1문의 기능 푸가, ] 디자인작업

--- src/main/java/com/devcamp/tripssoda/service/AdminBoardService.java
+++ src/main/java/com/devcamp/tripssoda/service/AdminBoardService.java
@@ -1,6 +1,7 @@
 package com.devcamp.tripssoda.service;
 
 import com.devcamp.tripssoda.dto.CombinedBoardDto;
+import com.devcamp.tripssoda.dto.InquiryDto;
 import com.devcamp.tripssoda.dto.SearchCondition;
 
 import java.util.List;
@@ -22,10 +23,12 @@ public interface AdminBoardService {
 
     boolean modify(CombinedBoardDto combinedBoardDto) throws Exception;
 
-//    List<CombinedBoardDto> getSearchResultPage(SearchCondition sc) throws Exception;
     List<CombinedBoardDto> getSearchResultPage(SearchCondition sc, String menuCode) throws Exception;
 
-//    int getSearchResultCnt(SearchCondition sc) throws Exception;
     int getSearchResultCnt(SearchCondition sc, String menuCode) throws Exception;
 
+
+    int selectAllInquiryCnt();
+
+    List<InquiryDto> selectAllInquiry(SearchCondition sc);
 }
